c89d668e56ec6664a386ba3f758203890457e751
Kanan Ismayilzada <kenanismayilzade12@gmail.com>
2024-07-28 13:59:21 +0300

    session, extensions, cart
    session, extensions, cart

---
+++ AspNetCoreMvc-eTicaret-MovieSales/Controllers/CartController.cs
@@ -0,0 +1,46 @@
+﻿using AspNetCoreMvc_eTicaret_MovieSales.Extensions;
+using AspNetCoreMvc_eTicaret_MovieSales.Interfaces;
+using AspNetCoreMvc_eTicaret_MovieSales.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMvc_eTicaret_MovieSales.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IMovieRepository _movieRepo;
+
+        public CartController(IMovieRepository movieRepo)
+        {
+            _movieRepo = movieRepo;
+        }
+
+        List<CartItem> cart = new List<CartItem>();   //sepet
+        CartItem cartItem = new CartItem();           //siparis
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add(int id, int Adet) 
+        {
+            var movie = _movieRepo.Get(id); //siparis edilecek movie
+            cart = GetCart();               //sepetimi istiyorum (ilk olarak bos sepet)
+            cartItem.MovieId = movie.Id;    //siparis olusturuyoruz
+            cartItem.MovieName = movie.Name;
+            cartItem.MovieQuantity = Adet;
+            cartItem.MoviePrice=movie.Price;
+            cartItem.AddToCart(cart, cartItem);
+            SetCart(cart);
+            return RedirectToAction("Index");
+        }
+        public void SetCart(List<CartItem> sepet) //sepet kayit (json formatta)
+        {
+            HttpContext.Session.SetJson("sepet", sepet);
+        }
+        public List<CartItem> GetCart() //kayitli sepeti  getir. (text formatta)
+        {
+            var sepet = HttpContext.Session.GetJson<List<CartItem>>("sepet") ?? new List<CartItem>();
+            //ilk basta sepet olmadigindan sepet null gelir bize bos sepet dondurur
+            return sepet;
+        }
+    }
+}
---
+++ AspNetCoreMvc-eTicaret-MovieSales/Models/CartItem.cs
@@ -0,0 +1,41 @@
+﻿namespace AspNetCoreMvc_eTicaret_MovieSales.Models
+{
+    public class CartItem
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public int MovieQuantity { get; set; }
+        public decimal MoviePrice { get; set; }
+
+        public List<CartItem> AddToCart(List<CartItem> cart,CartItem cartItem)
+        {
+            //if(cart.Any(c=>c.MovieId == cartItem.MovieId))
+            var item = cart.Find(c=>c.MovieId == cartItem.MovieId);
+            if (item != null)
+            {
+                item.MovieQuantity += cartItem.MovieQuantity;
+            }
+            else 
+            {
+                cart.Add(cartItem);
+            }
+            return cart;
+        }
+        public List<CartItem> DeleteFromCart(List<CartItem> cart, int id) 
+        { 
+            cart.RemoveAll(c=>c.MovieId == id);
+            return cart;
+        }
+        public int TotalQuantity(List<CartItem> cart) 
+        {
+            int total = cart.Sum(c=>c.MovieQuantity);
+            return total;
+        }
+        public decimal TotalPrice(List<CartItem> cart) 
+        {
+            decimal total = cart.Sum(c => c.MovieQuantity*c.MoviePrice);
+            return total;
+        }
+
+    }
+}
